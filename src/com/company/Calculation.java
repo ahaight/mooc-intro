5367c1773ab4d44b3b9f01d0ab16ac0e59b64206
Aaron Haight <ahahaight@infinata.com>
2016-10-07 17:01:24 -0400

    added Calculation java file

---
+++ src/com/company/Calculation.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by aaronhaight on 10/7/16.
+ */
+public class Calculation {
+
+    public static void main(String[] args) {
+        double first = 192;
+        double second = 398;
+
+        double sum = first / second;
+
+        System.out.println(sum);
+    }
+}
